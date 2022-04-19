ef7a947bf6c9494e714957fd517168f7d444e8c9
IronVeil <46629504+IronVeil@users.noreply.github.com>
2022-04-19 17:01:47 +0100

    Created blank settings file

---
+++ set/blank.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# SYSTEM
+system_vm=
+system_hostname=
+system_user=
+system_pass
+system_root_pass=
+system_kernel=
+
+# DISK
+disk_name=
+disk_type=
+disk_dir=
+
+# ENCRYPTION
+crypt=
+crypt_name="cryptsystem"
+crypt_partition=/dev/mapper/$crypt_name
+crypt_password=
+
+# PARTITIONS
+partition_layout=
+partition_bios=
+partition_boot=
+partition_root=
+partition_root_format=
+
+# PACKAGES
+packages=
