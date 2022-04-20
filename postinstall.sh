3374962f6f80cf37630a455ad1552d6461612444
IronVeil <46629504+IronVeil@users.noreply.github.com>
2022-04-20 17:38:17 +0100

    Added noconfirm

--- postinstall.sh
+++ postinstall.sh
@@ -74,7 +74,7 @@ sed -i "36,37s/#//" /etc/pacman.conf
 sed -i "94,95s/#//" /etc/pacman.conf
 
 # Reflector
-pacman -Sy reflector
+pacman -Sy --noconfirm reflector
 
 reflector -c GB --sort rate --save /etc/pacman.d/mirrorlist
 systemctl enable reflector.timer
